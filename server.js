ad132b39dd4c36fa06a82142cdb4ecaae8d55428
jborruel308@me.com <jborruel308@me.com>
2024-09-22 21:11:50 -0700

    controllers setup, nav bar partials setup, home page with nav bar setup

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
+
 const authController = require('./controllers/auth.js');
+const foodsController = require('//controllers/foods.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -20,6 +25,7 @@ mongoose.connection.on('connected', () => {
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
 // app.use(morgan('dev'));
+
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -27,6 +33,10 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(passUserToView)
+app.use ('/auth', authController);
+app.use(isSignedIn);
+app.use('/users/:userId/foods', foodsController);
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {
